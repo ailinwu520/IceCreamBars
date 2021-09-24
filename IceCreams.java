741e6b385aae4312f914676539565de3a231f8a7
Lin <81331908+ailinwu520@users.noreply.github.com>
2021-09-25 06:15:33 +1000

    Create IceCreams.java

---
+++ IceCreams.java
@@ -0,0 +1,19 @@
+/**
+ * @author
+ */
+public class IceCreams {
+    /**
+     * find the maximum ice cream bars you can buy
+     * @param costs array of costs of all ice cream bars, unsorted
+     * @param dollars the total amount of dollars you can use to buy the ice cream 
+     * return how many ice cream can buy with the money
+     */
+    public void maxIceCreams(int[] costs, int dollars) {
+        //TODO complete this function
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public static void main(String[] args) {
+        //test your code here
+    }
+}
